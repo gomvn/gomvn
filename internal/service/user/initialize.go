fa7847c607f080f9f22d001335a74f19e292aec3
Lukas Povoda <lupo112@gmail.com>
2020-06-06 15:07:06 +0200

    Initialize: print username

--- internal/service/user/initialize.go
+++ internal/service/user/initialize.go
@@ -15,12 +15,13 @@ func Initialize(db *gorm.DB, us *Service) error {
 	if count == 0 {
 		log.Println("Initializing first user...")
 
-		user, token, err := us.Create("admin", true,true, []string{"/"})
+		user, token, err := us.Create("admin", true, true, []string{"/"})
 		if err != nil {
 			return err
 		}
 
 		log.Printf("USER ID: %d\n", user.ID)
+		log.Printf("USERNAME: %s\n", user.Name)
 		log.Printf("TOKEN: %s\n", token)
 	}
 
